09aae06c112bc3f7b2995fc60c8ccb0de3b39e6b
SemperCuriosus <hulse@hey.com>
2022-11-30 08:57:42 -0500

    added a class and a slick new method to set the data globally.

--- file_organizer.py
+++ file_organizer.py
@@ -4,46 +4,65 @@ import shutil
 from zipfile import ZipFile
 import json
 
+class Settings:
+    # def __init__(self, _file_source, _file_desination, _log_file_path):
+    #     self._file_source = _file_source
+    #     self._file_desination = _file_desination
+    #     self._log_file_path = _log_file_path
+    __conf = {
+        "file_source":""
+        , "file_destination":""
+        , "log_path":""
+    }
 
-__file_source = ""
-__file_desination = ""
-__log_file_path = ""
+    __setters = ["file_source","file_destination", "log_path"]
 
+    @staticmethod
+    def config(name):
+        return Settings.__conf[name]
 
-
+    @staticmethod
+    def set(name, value):
+        if (name in Settings.__setters):
+            Settings.__conf[name] = value
+        
 def __main__():
     # main file call
-    _file_source = ""
-    _file_destination = ""
+    # _file_source = ""
+    # _file_destination = ""
 
     GetSettings()
 
-    print(__file_source)
-    print(__file_desination)
-    print(__log_file_path)
+    # print(Settings.config("file_source"))
+    # print(Settings.config("file_destination"))
+    # print(Settings.config("log_path"))
 
-    DoesPathExist(_file_source)
-    DoesPathExist(_file_destination)
+    if ((Settings.config("file_source") != "") and (Settings.config("file_destination") != "") and (Settings.config("log_path") != "")):
 
-    IsPathZipped(path=_file_source)
+        DoesPathExist(Settings.config("file_source"))
+        DoesPathExist(Settings.config("file_destination"))
 
-    WalkDirectory(file_source=_file_source, file_destination=_file_destination)
+        IsPathZipped(path=Settings.config("file_source"))
+
+        WalkDirectory(file_source=Settings.config("file_source"), file_destination=Settings.config("file_destination"))
 
 ## end : [__main__]
 
 
 def GetSettings():
-    __file_source = ""
-    __file_desination = ""
-    __log_file_path = ""
 
     try:
         with open("./configurations/appsettings.json", "r") as config_file:
-            config = json.loads(config_file)
-            _file_source = config["settings"][1]["Source_Path"]
-            _file_desination = config["settings"][1]["Target_Path"]
-            _log_file_path = config["settings"][1]["Source_Path"]
+            text = config_file.read()
+            config = json.loads(text)
+            _file_source = config["settings"]["source_path"]
+            _file_desination = config["settings"]["target_path"]
+            _log_file_path = config["settings"]["log_path"]
 
+            Settings.set("file_source", _file_source)
+            Settings.set("file_destination", _file_desination) 
+            Settings.set("log_path", _log_file_path)
+            
             LogToFile("Loaded configuration file.")
 
     except Exception as e:
@@ -56,7 +75,7 @@ def GetSettings():
 
 def DoesPathExist(path):
     if (not(os.path.exists(path))):
-        # print(f"The location [{path}] does not exist.")
+        LogToFile(f"The location [{path}] does not exist.")
         CreateMissingPath(path)
 
 ## end : [DoesPathExist]
@@ -75,20 +94,21 @@ def CreateMissingPath(path):
 
 
 def LogToFile(text_to_write):
-    _log_file_path = ""
+    # _log_file_path = ""
     _date = SetDate()
     _log_file_name = f"{_date}_log.txt"
     
-    if (_log_file_name != "" and _log_file_path != ""):
+    if (_log_file_name != "" and Settings.config("log_path") != ""):
 
-        _log_full_path = JoinPath(_log_file_path, _log_file_name) 
+        _log_full_path = JoinPath(Settings.config("log_path"), _log_file_name) 
 
         try:
             with open (str(_log_full_path), "a") as file:
                 file.writelines("= = = = = = = = = = = = = = = = = = = = =")
                 file.writelines('\n')
                 file.writelines(f"Log File : {_date}")
-                file.writelines(f"error text : {text_to_write}")
+                file.writelines('\n')
+                file.writelines(f"text : {text_to_write}")
                 file.writelines('\n')
                 file.writelines("= = = = = = = = = = = = = = = = = = = = =")
                 file.writelines('\n')
